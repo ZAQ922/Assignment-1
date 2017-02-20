f65af4e1d644682b9ca46ed8887accccbe6b23f6
ZAQ922 <zaq922@bellsouth.net>
2017-02-20 12:24:54 -0600

    Create CoolName.cpp

---
+++ CoolName.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomain>
+using namespace std;
+
+int main(){
+  int Input = 0;
+  cout << "Choose: /t1 = BMI /t2 = Retirement /t3 = Distance /t4 = Email" << endl;
+  switch(Input){
+    case '1'://BMI
+      
+      break;
+    case '2'://Retirement
+    
+      break;
+    case '3'://Distance
+    
+      break;
+    case '4'://Email
+      
+      break;
+    default:
+      cout << "Wrong Choice." << endl;
+      break;
+  }
+
+  return 0;
+}
