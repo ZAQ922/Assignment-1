5d5fa0a69ffa090e566544f501a6778551e91d3d
sommerjj <sommerjj473@gmail.com>
2017-02-21 13:00:18 -0600

    Add files via upload

---
+++ distanceFormulaTests.py
@@ -0,0 +1,19 @@
+import unittest
+
+class distanceFormulaTests(unittest.TestCase):
+	
+	def testNegativeInputs():
+		assertTrue(False)
+
+	def testLargeInputs()
+		assertTrue(False)
+
+	def normalInputs()
+		assertTrue(False)
+
+def main():
+	unittest.main()
+
+
+if __name__ == 'main__':
+	main()
