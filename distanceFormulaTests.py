4bba99351644a75f2189332843c63b8815de5390
Joseph Sommer <jjs451@msstate.edu>
2017-02-21 16:33:21 -0600

    Added more failing tests

--- distanceFormulaTests.py
+++ distanceFormulaTests.py
@@ -1,11 +1,18 @@
 import unittest
+from math import sqrt
 
 def distanceFormula(x1, y1, x2, y2):
 	return 2
 
 class distanceFormulaTests(unittest.TestCase):
 	def test_normalInputs(self):
-		self.assertEqual(distanceFormula(1,1,1,1), 2)
+		self.assertEqual(distanceFormula(1,1,1,1), sqrt(4))
+
+	def test_bigInputs(self):
+		self.assertEqual(distanceFormula(200, 200, 200, 200), sqrt(80000))
+
+	def test_negativeInputs(self):
+		self.assertEqual(distanceFormula(-5, -5, -5, -5), sqrt(50))
 
 if __name__ == 'main__':
 	unittest.main()
