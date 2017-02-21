c9b03723de99073bb4475931da7f02c6e33f6817
Joseph Sommer <jjs451@msstate.edu>
2017-02-21 13:43:58 -0600

    Passed 1 test

--- distanceFormulaTests.py
+++ distanceFormulaTests.py
@@ -1,9 +1,11 @@
 import unittest
 
+def distanceFormula(x1, y1, x2, y2):
+	return 2
 
 class distanceFormulaTests(unittest.TestCase):
 	def test_normalInputs(self):
-		self.assertEqual(1, 0)
+		self.assertEqual(distanceFormula(1,1,1,1), 2)
 
 if __name__ == 'main__':
 	unittest.main()
