e14ac41f627044a54c5dceb8b67abe6f841af2d8
ZAQ922 <zaq922@bellsouth.net>
2017-02-22 16:13:27 -0600

    Main Menu
    Tested all functionality. Nothing but numbers from 1-4 work.

--- MainMenu.py
+++ MainMenu.py
@@ -1,25 +1,33 @@
 
+class main():
+    def mainMenu(self):
+        print('1  :   BMI Calculator')
+        print('2  :   Retirement Calculator')
+        print('3  :   Distance Formula')
+        print('4  :   Email Verifier')
+        """while loop for continual menu options?"""
+        try:
+            choice = int(input("Select an application number: "))
+        except ValueError:
+            print('What you input was not an integer!')
+            return 0
+        if (isinstance(choice, int)):
+            if (1 <= choice <= 4):
+                if choice == 1:
+                    print(choice, ": BMI Calculator chosen")
+                if choice == 2:
+                    print(choice, ": Retirement chosen")
 
-def main():
-    """while loop for continual menu options?"""
-    print('1  :   BMI Calculator')
-    print('2  :   Retirement Calculator')
-    print('3  :   Distance Formula')
-    print('4  :   Email Verifier')
-    choice = input("Select an application number: ")
+                if choice == 3:
+                    print(choice, ": Distance chosen")
 
-    if isinstance(choice, int) and (1 <= choice <= 4):
-        if choice is 1:
-            print(choice, ": BMI Calculator chosen")
-        if choice is 2:
-            print(choice, ": Retirement chosen")
-        if choice is 3:
-            print(choice, ": Distance chosen")
-        if choice is 4:
-            print(choice, ": Email chosen")
-    else:
-        print('Please choose a number from 1 to 4.')
+                if choice == 4:
+                    print(choice, ": Email chosen")
+            else:
+                print('That number is not an option: 1, 2, 3, or 4')
+        else:
+            print('Something went wrong.')
+        return 0
 
 
-if __name__ == 'main__':
-    main()
+main().mainMenu()
