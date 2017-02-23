2cf8beef4056e1d97eaa8165e9c38494d4e4949d
ZAQ922 <zaq922@bellsouth.net>
2017-02-23 13:40:29 -0600

    MainMenu Update
    Added while loop into the BMI Calculator, cannot leave until done with it.

--- MainMenu.py
+++ MainMenu.py
@@ -14,42 +14,49 @@ class main():
         if (isinstance(choice, int)):
             if (1 <= choice <= 4):
                 if choice == 1:
-                    try:
-                        feet = int(input("Please input Feet: "))
-                    except ValueError:
-                        print('Feet must be an Integer!')
-                    try:
-                        inches = int(input("Please input Inches: "))
-                    except ValueError:
-                        print('Inches must be an Integer!')
-                    try:
-                        pounds = int(input("Please input Pounds: "))
-                    except ValueError:
-                        print('Pounds must be an Integer')
-                    if (isinstance(feet, int) and (0 < feet <= 1000)):
-                        if (isinstance(inches, int) and (0 < inches < 12)):
-                            if (isinstance(pounds, int) and (0 < pounds <= 2000)):
-                                kg = pounds * 0.45
-                                totalInches = ((feet * 12) + inches)
-                                meters = totalInches * 0.025
-                                bmi = round((kg/(meters * meters)), 1)
-                                print('BMI = ', bmi)
-                                if(bmi < 0):
-                                    print('BMI is a negative number')
-                                if(0 <= bmi < 18.5):
-                                    print('Underweight')
-                                if(18.5 <= bmi < 25):
-                                    print('Normal')
-                                if(25 <= bmi < 30):
-                                    print('Overweight')
-                                if(30 <= bmi):
-                                    print('Obese')
+                    bob = True
+                    while bob:
+                        try:
+                            feet = int(input("Please input Feet: "))
+                        except ValueError:
+                            print('Feet must be an Integer!')
+                        if (isinstance(feet, int) and (0 < feet <= 10)):
+                            try:
+                                inches = int(input("Please input Inches: "))
+                            except ValueError:
+                                print('Inches must be an Integer!')
+                            if (isinstance(inches, int) and (0 < inches < 12)):
+                                try:
+                                    pounds = int(input("Please input Pounds: "))
+                                except ValueError:
+                                    print('Pounds must be an Integer')
+                                if (isinstance(pounds, int) and (0 < pounds <= 1000)):
+                                    kg = pounds * 0.45
+                                    totalInches = ((feet * 12) + inches)
+                                    meters = totalInches * 0.025
+                                    bmi = round((kg/(meters * meters)), 1)
+                                    print('BMI = ', bmi)
+                                    if(bmi < 0):
+                                        print('BMI is a negative number')
+                                        bob = False
+                                    if(0 <= bmi < 18.5):
+                                        print('Underweight')
+                                        bob = False
+                                    if(18.5 <= bmi < 25):
+                                        print('Normal')
+                                        bob = False
+                                    if(25 <= bmi < 30):
+                                        print('Overweight')
+                                        bob = False
+                                    if(30 <= bmi):
+                                        print('Obese')
+                                        bob = False
+                                else:
+                                    print('0 < Pounds <= 2000!')
                             else:
-                                print('0 < Pounds <= 2000!')
+                                print('0 < Inches < 12!')
                         else:
-                            print('0 < Inches < 12!')
-                    else:
-                        print('0 < Feet <= 1000!')
+                            print('0 < Feet <= 1000!')
 
                 if choice == 2:
                     print(choice, ": Retirement chosen")
