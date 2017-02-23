8cc256592e21b51680701b01f338595982728cf8
ZAQ922 <zaq922@bellsouth.net>
2017-02-23 13:27:42 -0600

    BMI Implemented
    The BMI Calculator is implemented and works correctly. The other 3 are not implemented.

--- MainMenu.py
+++ MainMenu.py
@@ -14,7 +14,43 @@ class main():
         if (isinstance(choice, int)):
             if (1 <= choice <= 4):
                 if choice == 1:
-                    print(choice, ": BMI Calculator chosen")
+                    try:
+                        feet = int(input("Please input Feet: "))
+                    except ValueError:
+                        print('Feet must be an Integer!')
+                    try:
+                        inches = int(input("Please input Inches: "))
+                    except ValueError:
+                        print('Inches must be an Integer!')
+                    try:
+                        pounds = int(input("Please input Pounds: "))
+                    except ValueError:
+                        print('Pounds must be an Integer')
+                    if (isinstance(feet, int) and (0 < feet <= 1000)):
+                        if (isinstance(inches, int) and (0 < inches < 12)):
+                            if (isinstance(pounds, int) and (0 < pounds <= 2000)):
+                                kg = pounds * 0.45
+                                totalInches = ((feet * 12) + inches)
+                                meters = totalInches * 0.025
+                                bmi = round((kg/(meters * meters)), 1)
+                                print('BMI = ', bmi)
+                                if(bmi < 0):
+                                    print('BMI is a negative number')
+                                if(0 <= bmi < 18.5):
+                                    print('Underweight')
+                                if(18.5 <= bmi < 25):
+                                    print('Normal')
+                                if(25 <= bmi < 30):
+                                    print('Overweight')
+                                if(30 <= bmi):
+                                    print('Obese')
+                            else:
+                                print('0 < Pounds <= 2000!')
+                        else:
+                            print('0 < Inches < 12!')
+                    else:
+                        print('0 < Feet <= 1000!')
+
                 if choice == 2:
                     print(choice, ": Retirement chosen")
 
