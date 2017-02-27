4d7431cee86363b1496c415e39f4e0fa5e7881e7
krlsmnk <krlsmnk@yahoo.com>
2017-02-26 22:00:30 -0600

    Add files via upload

--- MainMenu.py
+++ MainMenu.py
@@ -67,7 +67,43 @@ class main():
 
                 if choice == 2:
                     print(choice, ": Retirement chosen")
-                    input("Press any key to exit")
+
+                    frank = True
+                    while frank:
+                        # Input user's current age
+                        currentAge = input('Enter your current age: ')
+                        currentAge = int(currentAge)
+                        if currentAge > 0:
+                            # annual salary
+                            annualSalary = input('Enter your annual salary: ')
+                            annualSalary = int(annualSalary)
+                            if annualSalary > 0:
+                                # percentage saved (savings matched by employer - so double amount saved)
+                                percentageSaved = input('Enter your percentage saved as an integer value: ')
+                                percentageSaved = int(percentageSaved)
+                                percentageSaved *= 2
+                                if percentageSaved > 0:
+                                    # Input desired retirement savings goal
+                                    savingsGoal = input('Enter your savings goal: ')
+                                    savingsGoal = int(savingsGoal)
+
+                                    # Output what age savings goal will be met.
+                                    ageGoalMet = savingsGoal / (annualSalary * percentageSaved / 100) + currentAge
+
+                                    # You can assume death at 100 years (therefore, indicate if the savings goal is not met).
+                                    if ageGoalMet > 99:
+                                        print('Savings goal not met... Happy 100th?')
+                                        frank = False
+                                    else:
+                                        print('You will meet your savings goal at the age of: ', ageGoalMet)
+                                        frank = False
+                                else:
+                                    print("Savings > 0!")
+                            else:
+                                print("annual salary > 0!")
+                        else:
+                            print("current age > 0!")
+
 
                 if choice == 3:
                     print(choice, ": Distance chosen")
