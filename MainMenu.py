7d07d082ff4857a1515625c603b1c374d681a456
ZAQ922 <zaq922@bellsouth.net>
2017-03-02 23:00:16 -0600

    Update MainMenu.py

--- MainMenu.py
+++ MainMenu.py
@@ -124,7 +124,34 @@ class main():
                         input("#############################################################################")
 
                     if choice == 4:
-                        print(choice, ": Email chosen")
+                        loop4 = True
+                        email = input('Input email to be tested: ')
+                        while loop4:
+                            try:
+                                useremail = re.search(r'(.*)@(.*)\.(.*)', email, re.I)
+                                useremailver1 = re.search(useremail.group(1), email, re.I)
+
+                                removespecialchar = re.compile('[\[{}()\]\\\*]')
+
+                                a = removespecialchar.sub('', useremail.group(1))
+
+                                b = removespecialchar.sub('', useremail.group(2))
+
+                                c = removespecialchar.sub('', useremail.group(3))
+
+                                    if not ((re.match(('[\[{}()\]\\\*]'), email))):
+                                        if (len(c) <= 3):
+                                            print(email, " is a valid email!")
+                                            loop4 = False
+                                            return True
+                                        else:
+                                            print(c, "is and invalid domain extention: it must be three letters or less.")
+                                            loop4 = False
+                                            return False
+
+                            except:
+                                print(email, "is not a valid email")
+                                return False
                         input("#############################################################################")
                 else:
                     print('That number is not an option: 1, 2, 3, or 4')
